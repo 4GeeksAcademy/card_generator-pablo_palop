10a550af355ec76e59094ad7541825eecdcd17b0
pablopalop <130085149+pablopalop@users.noreply.github.com>
2025-02-28 20:28:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const cardNumber = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+// const cardSuit = ["Heart", "Spade", "Club", "Diamond"]
+const cardSuit = ["♥", "♠", "♣", "♦"];
+
+window.onload = function () {
+  let randomNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuit = Math.floor(Math.random() * cardSuit.length);
+  let numberToShow = cardNumber[randomNumber];
+  let suitToShow = cardSuit[randomSuit];
+  document.getElementById("card-number").textContent = numberToShow;
+  document.getElementById("top-suit").textContent = suitToShow;
+  document.getElementById("bottom-suit").textContent = suitToShow;
+
+  if (suitToShow === "♥" || suitToShow === "♦") {
+    document.getElementById("top-suit").style.color = "red";
+    document.getElementById("bottom-suit").style.color = "red";
+    document.getElementById("card-number").style.color = "red";
+  } else {
+    document.getElementById("top-suit").style.color = "black";
+    document.getElementById("bottom-suit").style.color = "black";
+    document.getElementById("card-number").style.color = "black";
+  }
 };
+
+
+const button = document.getElementById("btn")
+function generateCard() {
+  let randomNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuit = Math.floor(Math.random() * cardSuit.length);
+  let numberToShow = cardNumber[randomNumber];
+  let suitToShow = cardSuit[randomSuit];
+  document.getElementById("card-number").textContent = numberToShow;
+  document.getElementById("top-suit").textContent = suitToShow;
+  document.getElementById("bottom-suit").textContent = suitToShow;
+
+  if (suitToShow === "♥" || suitToShow === "♦") {
+    document.getElementById("top-suit").style.color = "red";
+    document.getElementById("bottom-suit").style.color = "red";
+    document.getElementById("card-number").style.color = "red";
+  } else {
+    document.getElementById("top-suit").style.color = "black";
+    document.getElementById("bottom-suit").style.color = "black";
+    document.getElementById("card-number").style.color = "black";
+  }
+}
+button.addEventListener('click', generateCard);
+
+
+
+let intervalId;
+
+function autoGenerate(){
+  intervalId = setInterval(generateCard,1000);
+}
+
+function stopGenerator(){
+  clearInterval(intervalId);
+}
+
+document.getElementById("btnStart").addEventListener('click', autoGenerate);
+document.getElementById("btnStop").addEventListener('click', stopGenerator);
